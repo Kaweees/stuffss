dee1d3650afabe7d6b7ab7f3cc076efcf63b5f98
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-06-06 22:17:11 -0700

    Add stuff

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
         origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE']
     }
 });
 const bodyParser = require('body-parser');
@@ -31,24 +32,24 @@ app.use(cors({
   }
 ))
 
-// Initializing database:
+// Initializing database :
 initializeApp({
-    credential: cert(
-      {
-        "type": process.env.FIREBASE_TYPE,
-        "project_id": process.env.FIREBASE_PROJECT_ID,
-        "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-        "client_id": process.env.FIREBASE_CLIENT_ID,
-        "auth_uri": process.env.FIREBASE_AUTH_URI,
-        "token_uri": process.env.FIREBASE_TOKEN_URI,
-        "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-        "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
-        "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
-      }
-    )
-}); 
+  credential: cert(
+    {
+      "type": process.env.FIREBASE_TYPE,
+      "project_id": process.env.FIREBASE_PROJECT_ID,
+      "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+      "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+      "client_id": process.env.FIREBASE_CLIENT_ID,
+      "auth_uri": process.env.FIREBASE_AUTH_URI,
+      "token_uri": process.env.FIREBASE_TOKEN_URI,
+      "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+      "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
+    }
+  )
+});
 
 const db = getFirestore();
 console.log("Database initialized.");
